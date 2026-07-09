35f585ba5c09c65891c591939677a1e75dcdb9e0
endokard <45415540+endokard@users.noreply.github.com>
2026-07-09 10:07:01 +0300

    Create index.php

---
+++ edemtour/index.php
@@ -0,0 +1,19 @@
+<?php
+
+defined('_JEXEC') or die;
+
+?>
+<!DOCTYPE html>
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<head>
+    <jdoc:include type="head" />
+</head>
+
+<body>
+
+<jdoc:include type="message" />
+
+<jdoc:include type="component" />
+
+</body>
+</html>
