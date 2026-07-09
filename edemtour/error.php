4fa71cdf58f3ff560b5f1adad2e242924abc1088
endokard <45415540+endokard@users.noreply.github.com>
2026-07-09 10:07:34 +0300

    Create error.php

---
+++ edemtour/error.php
@@ -0,0 +1,19 @@
+<?php
+
+defined('_JEXEC') or die;
+
+?>
+<!DOCTYPE html>
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<head>
+    <jdoc:include type="head" />
+</head>
+
+<body>
+
+<h1>Error</h1>
+
+<jdoc:include type="message" />
+
+</body>
+</html>
