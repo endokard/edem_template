9c02bb0d5aa866064fd35c83a4203a529f7686f4
endokard <45415540+endokard@users.noreply.github.com>
2026-07-09 10:07:50 +0300

    Create offline.php

---
+++ edemtour/offline.php
@@ -0,0 +1,21 @@
+<?php
+
+defined('_JEXEC') or die;
+
+?>
+<!DOCTYPE html>
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<head>
+    <jdoc:include type="head" />
+</head>
+
+<body>
+
+<h1>Site Offline</h1>
+
+<jdoc:include type="message" />
+
+<jdoc:include type="component" />
+
+</body>
+</html>
